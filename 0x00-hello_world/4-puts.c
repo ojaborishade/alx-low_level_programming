838c3dfa712654cae1c67c6ee3575e464c2c3171
Olufunmilayo <olufunmilayoj.aborishade@gmail.com>
2022-10-19 22:30:58 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
 /**
- *
- * * main - Prints ""Programming is like building a multilingual puzzle"
- *
- * * Return: 0
- *
- * */
+ * main - prints ""Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
 
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-                return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
