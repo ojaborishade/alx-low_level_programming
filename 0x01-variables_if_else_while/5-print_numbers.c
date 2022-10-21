39d079fa13b49957d3c03e85fb2f821b826555e3
Olufunmilayo <olufunmilayoj.aborishade@gmail.com>
2022-10-21 12:58:29 -0700

    Single digits of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - prints all single digit numbers of base 10 starting from 0
+* Return: 0
+*/
+
+int main(void)
+{
+	char i;
+
+	i = 9;
+
+	while (i <= 10)
+	{
+	putchar(i);
+	i++;
+	}
+	
+	putchar('\n');
+	return (0);
+}
+
