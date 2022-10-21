96c8ee19b055f7693fd908c5befc2bc5db184c65
Olufunmilayo <olufunmilayoj.aborishade@gmail.com>
2022-10-21 12:02:58 -0700

    Lowercase alphabets followed by a new line

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+* main - prints the alphabets in lowercase followed by a new line.
+* Return: 0
+*/ 
+
+int main(void)
+{
+	char i;
+
+	i = 'a';
+
+	while (i <= 'z')
+	{
+	putchar(i);
+	i++;
+	if (i == 'e')
+	{
+	i++;
+	}
+	else if (i == 'q')
+	{
+	i++;
+	}
+	}
+
+	putchar('\n');
+	return (0);
+}
