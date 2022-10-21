2d7e9c9fda29da184ac0ed41de6fa40db939a0b1
Olufunmilayo <olufunmilayoj.aborishade@gmail.com>
2022-10-21 11:19:26 -0700

    Lowercase alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - prints the alphabet
+* Return: 0
+*/
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
