9230ef89345fee0ebfa8af4d9e20ec662a0e3e9a
Olufunmilayo <olufunmilayoj.aborishade@gmail.com>
2022-10-21 06:59:51 -0700

    Sixth Task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ int main(void)
 
 	printf("Size of a char: 1 byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of an int: 4 byte(s)\n", (unsigned long)sizeof(i));
-	printf("Size of a long int: 4 byte(s)\n", (unsigned long)sizeof (li));
+	printf("Size of a long int: 4 byte(s)\n", (unsigned long)sizeof(li));
 	printf("Size of a long long int: 8 byte(s)\n", (unsigned long)sizeof(lli));
 	printf("Size of a float: 4 byte(s)\n", (unsigned long)sizeof(f));
 
