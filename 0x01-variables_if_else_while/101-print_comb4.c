497becf2b751ad993adbcfadfffd98dc49d1aa1e
Olufunmilayo <olufunmilayoj.aborishade@gmail.com>
2022-10-23 13:21:21 -0700

    Prints all possible different combination of 3 digitnumber

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+* main - prints all possible different combination of 3 digit numbers.
+* Return: 0
+*/
+
+int main(void)
+{
+	int i = 1;
+
+	while (i < 90)
+	{
+		if ((i % 10) < (i / 10))
+		{
+			i = i + 1 + 1 * (i / 10);
+		}
+		putchar((i / 10) + '0');
+		putchar((i % 10) + '0');
+		putchar((i % 100) + '1');
+		if (i < 89)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	
+	putchar('\n');
+	return (0);
+}
