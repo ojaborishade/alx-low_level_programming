c3e277952bb1bb979ad004962c6ec68159759aba
Olufunmilayo <olufunmilayoj.aborishade@gmail.com>
2022-10-24 15:47:53 -0700

    Reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main -  reset_to_98 - To change the value at a memory address.
+* Return: 0
+*/
+
+int main(void)
+{
+	int n;
+
+	n = 402
+	printf("n=%d\n", n);
+	reset_to_98(&n);
+	printf("n=%d\n", n);
+	return (0);
+}
