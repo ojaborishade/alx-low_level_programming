83e85a687c3568352b28c9cd369981ff6b5dee7f
Olufunmilayo <olufunmilayoj.aborishade@gmail.com>
2022-10-26 10:46:57 -0700

    Prints putchar followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,10 +11,8 @@ int main(void)
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
-}
+        _putchar('a');
+        _putchar('r');
+        _putchar('\n');
+        return (0);
+}	
