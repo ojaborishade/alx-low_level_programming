44435c954d921890dc68502ced2c69ea463d59f6
Olufunmilayo <olufunmilayoj.aborishade@gmail.com>
2022-10-27 03:48:04 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,10 +2,10 @@
 
 /**
 * print_sign - print the sign of a num
-* @n: the num to be checked for
-* Return: 1 and prints + if n is greater than zero
-* 0 and prints 0 if n is zero
-* -1 and prints - if n is less than zero
+* @n: the num to be checked for sign
+* Return: 1 and prints + if n is greater than zero,
+* 0 and prints 0 if n is zero,
+* -1 and prints - if n is less than zero.
 */
 
 int print_sign(int n)
