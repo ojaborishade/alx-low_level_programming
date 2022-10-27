2ad88fa50f1c9bf84d392abb1f2703964a5dc642
Olufunmilayo <olufunmilayoj.aborishade@gmail.com>
2022-10-27 15:49:35 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 
 /**
  * jack_bauer - prints every minute of the day of
- * Jack Bauer, starting from 00:00 to 23:59.
+ *              Jack Bauer, starting from 00:00 to 23:59.
  */
 
 void jack_bauer(void)
@@ -17,7 +17,7 @@ void jack_bauer(void)
 			_putchar((hour % 10) + '0');
 			_putchar(':');
 			_putchar((minute / 10) + '0');
-			_putchar((minute % 10 + '0');
+			_putchar((minute % 10) + '0');
 			_putchar('\n');
 		}
 	}
