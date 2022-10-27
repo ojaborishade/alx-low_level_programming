9e18e4e7ec31fecd8978bff78c98aa3bef3ef32d
Olufunmilayo <olufunmilayoj.aborishade@gmail.com>
2022-10-26 18:30:17 -0700

    Prints _putchar followed by a new line.  commit message for your changes. Lines starting

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- *  main - Prints _putchar followed by a new line
- *  Return: 0
- */
+*  main - Prints _putchar followed by a new line
+*  Return: 0
+*/
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
