43dc8c442da790bdf86dff169a32b7e6e922ecff
Olufunmilayo <olufunmilayoj.aborishade@gmail.com>
2022-10-27 01:36:25 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,17 @@
 #include "main.h"
 
 /**
-* print_alphabet - Prints the alphabet.
+* print_alphabet - Prints all alphabet in lowercase followed by a new line
 * Return: 0
 */
 
 void print_alphabet(void)
 {
-	char letter = 'a';
-	
-	while (letter <= 'z')
-	{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
-		letter++;
-	}
 
 	_putchar('\n');
 	return (0);
-
 }
