01b9221e6c3dea40a7c77c5d7f2521b01c376403
Olufunmilayo <olufunmilayoj.aborishade@gmail.com>
2022-10-27 16:51:16 -0700

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * print_to_98 - prints all natural numbers from n to 98,
- * followed by a new line
+ *               followed by a new line
  * @n: the num to be counted
  */
 
@@ -18,6 +18,6 @@ void print_to_98(int n)
 	{
 		while (n > 98)
 			printf("%d, ", n--);
-		print("%d\n" n);
+		printf("%d\n" n);
 	}
 }
