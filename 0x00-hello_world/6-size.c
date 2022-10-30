997a05f52b2498f51443abb638aee2036febc827
Olufunmilayo <olufunmilayoj.aborishade@gmail.com>
2022-10-30 00:33:00 -0700

    Advance Task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ int main(void)
 	long long int lli;
 	float f;
 
-	fprintf("Size of a char: 1 byte(s)\n", (unsigned long)sizeof(c));
-	fprintf("%s" "Size of an int: 4 byte(s)\n", (unsigned long)sizeof(i));
-	fprintf(stderr, "%s" "Size of a long int: 4 byte(s)\n", (unsigned long)sizeof(li));
-	fprintf(stderr, "%s" "Size of a long long int: 8 byte(s)\n", (unsigned long)sizeof(lli));
-	fprintf(stderr, "%s" "Size of a float: 4 byte(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 
 	return (0);
 }
