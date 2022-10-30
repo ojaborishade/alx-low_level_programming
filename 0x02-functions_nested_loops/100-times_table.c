0629b2a73bccb1fdc55339afde35b5e0a2831889
Olufunmilayo <olufunmilayoj.aborishade@gmail.com>
2022-10-29 22:28:42 -0700

    Advance task 1

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,7 +2,7 @@
 
 /**
  * print_times_table - prints the n times table, starting with 0
- * @n: the value of timestable to be printed
+ * @n: the value of times_table to be printed
  */
 
 void print_times_table(int n)
