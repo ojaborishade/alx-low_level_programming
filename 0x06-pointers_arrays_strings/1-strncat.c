3c6c71564f31dd37ad32f8b7c6848d19aa098574
Olufunmilayo <olufunmilayoj.aborishade@gmail.com>
2022-10-30 22:06:41 -0700

    Task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strcat - joins two str with n bytes
+ * @dest: copy to
+ * @src: copy from
+ * @n: num of char to be copied
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src, int n)
+{
+	int i;
+	int l;
+
+	i = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	l = 0;
+	while (l < n && src[l] != '\0')
+	{
+		dest[i] = src[l];
+		i++;
+		l++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
